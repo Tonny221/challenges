fac3f02feda410989bc4a58af957af3132ce1a62
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2021-06-29 16:15:02 -0400

    menu js finished

--- responsive-travel-site/js/main.js
+++ responsive-travel-site/js/main.js
@@ -1,3 +1,32 @@
+//show menu
 const navMenu = document.getElementById('nav-menu'),
 navToggle = document.getElementById('nav-toggle'),
-navClose = document.getElementById('nav-close')
+navClose = document.getElementById('nav-close');
+
+//menu show
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    })
+}
+
+//menu close
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+//nav link part
+
+const navLink = document.querySelectorAll('.nav_link')// quando querer trabalhar com mais de um elemento usar o querySelectorAll
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //quando clicar no link remove a classe show-menu
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
