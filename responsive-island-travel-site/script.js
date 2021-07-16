41365ff526cebd61b0911ef3a528e4b610a44d6b
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2021-07-16 15:47:59 -0400

    islands popup done

--- responsive-island-travel-site/script.js
+++ responsive-island-travel-site/script.js
@@ -12,3 +12,21 @@ const showMenu = (toggleId, navId) =>{
 }
 
 showMenu('nav-toggle', 'nav-menu');
+
+//Pop Up
+
+const btnOpenVideo = document.querySelectorAll('.island_video-content');
+const islandPopup = document.getElementById('popup');
+
+function popUp(){
+    islandPopup.classList.add('show-popup')
+}
+
+//b é uma variavel criada de valor volatil para quantas vezes for necessário abrir o popup
+btnOpenVideo.forEach(b => b.addEventListener('click', popUp))
+
+const btnCloseVideo = document.getElementById('popup-close')
+
+btnCloseVideo.addEventListener('click', () => {
+    islandPopup.classList.remove('show-popup')
+})
